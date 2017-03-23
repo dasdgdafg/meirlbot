bbbdfa50104c08b72df86f0131d7182d771f19c6
dsdf <dasdgdafg@cock.li>
2017-03-22 20:20:57 -0400

    add timestamps to logging

--- logging.php
+++ logging.php
@@ -2,20 +2,21 @@
 
 function logMessage($msg, $alsoPrint=true)
 {
-    static $logFile = "log.txt";
     static $file = false;
     
     if (!is_resource($file))
     {
+        $dateName = date(DATE_ATOM);
+        $logFile = "logs/log-$dateName.txt";
+        $logFile = str_replace(":", "-", $logFile);
         echo "opening file $logFile\n";
-        $file = fopen($logFile, 'a', true);
+        $file = fopen($logFile, 'w', true);
     }
-    fwrite($file, $msg . "\n");
+    fwrite($file, date(DATE_ATOM) . "\t" . $msg . "\n");
     
     if ($alsoPrint)
     {
         echo $msg . "\n";
     }
 }
-
 ?>
