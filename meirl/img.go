9c5e9b082a8c13578b6d56589e4d77ade76776a9
dsdf <dasdgdafg@cock.li>
2017-04-02 13:50:48 -0400

    ran go fmt

--- meirl/img.go
+++ meirl/img.go
@@ -1,120 +1,122 @@
 package main
 
 import (
-    "log"
-    "regexp"
-    "math/rand"
-    "encoding/xml"
-    "net/http"
-    "strconv"
+	"encoding/xml"
+	"log"
+	"math/rand"
+	"net/http"
+	"regexp"
+	"strconv"
 )
 
 type CuteImage struct {
 	// no fields for now, cache stuff later
-    // maybe allow config per channel
+	// maybe allow config per channel
 }
 
 type response struct {
-    Count string `xml:"count,attr"`
-    Posts []post `xml:"post"`
+	Count string `xml:"count,attr"`
+	Posts []post `xml:"post"`
 }
 
 type post struct {
-    File string `xml:"file_url,attr"`
+	File string `xml:"file_url,attr"`
 }
 
-const baseUrl = "https://gelbooru.com/index.php?page=dapi&s=post&q=index&tags=";
+const baseUrl = "https://gelbooru.com/index.php?page=dapi&s=post&q=index&tags="
 
 var regexes = []*regexp.Regexp{regexp.MustCompile("(?i)(me irl)"),
-                               regexp.MustCompile("(?i)(me on the (?:left|right))"),
-                               regexp.MustCompile("(?i)(me being lewd)"),
-                               regexp.MustCompile("(?i)(me with tags) (.*)")}
+	regexp.MustCompile("(?i)(me on the (?:left|right))"),
+	regexp.MustCompile("(?i)(me being lewd)"),
+	regexp.MustCompile("(?i)(me with tags) (.*)")}
+
 // these must match the order of the regexes
 var tags = []string{"loli solo score:>5 rating:questionable",
-                    "loli multiple_girls score:>5 rating:questionable -large_breasts -1boy -multiple_boys",
-                    "loli solo score:>5 masturbation",
-                    ""}
+	"loli multiple_girls score:>5 rating:questionable -large_breasts -1boy -multiple_boys",
+	"loli solo score:>5 masturbation",
+	""}
+
 // to avoid looking up the count each time. it would be better to get these once and cache instead of hard coding
 var counts = []int{10000, 3500, 1500, 0}
 
 // returns (matching string, image url)
 func (c CuteImage) getImageForMessage(msg string) (string, string) {
-    for i, reg := range regexes {
-        if reg.MatchString(msg) {
-            matches := reg.FindStringSubmatch(msg)
-            matchingString := matches[1]
-            imageUrl := ""
-            // use matches[2] (user specified tags) if there are no tags
-            if tags[i] == "" && len(matches) > 2 {
-                imageUrl = c.getImage(counts[i], matches[2])
-            } else {
-                imageUrl = c.getImage(counts[i], tags[i])
-            }
-            return matchingString, imageUrl
-        }
-    }
-    log.Println("error determining image type for " + msg)
-    return "", ""
+	for i, reg := range regexes {
+		if reg.MatchString(msg) {
+			matches := reg.FindStringSubmatch(msg)
+			matchingString := matches[1]
+			imageUrl := ""
+			// use matches[2] (user specified tags) if there are no tags
+			if tags[i] == "" && len(matches) > 2 {
+				imageUrl = c.getImage(counts[i], matches[2])
+			} else {
+				imageUrl = c.getImage(counts[i], tags[i])
+			}
+			return matchingString, imageUrl
+		}
+	}
+	log.Println("error determining image type for " + msg)
+	return "", ""
 }
 
 func (c CuteImage) checkForMatch(msg string) bool {
-    for _, reg := range regexes {
-        if reg.MatchString(msg) {
-            return true
-        }
-    }
-    return false
+	for _, reg := range regexes {
+		if reg.MatchString(msg) {
+			return true
+		}
+	}
+	return false
 }
 
 func (c CuteImage) getImage(count int, tags string) string {
-    // fetch the count if we don't have it
-    if count < 1 {
-        count = c.getCount(tags)
-        if count < 1 {
-            return ""
-        }
-    }
-    pid := rand.Intn(count)
-    requestUrl := baseUrl + tags + "&limit=1&pid=" + strconv.Itoa(pid)
-    log.Println("getting image from " + requestUrl)
-    resp, err := http.Get(requestUrl)
-    if err != nil {
-        log.Println("error fetching image")
-        log.Println(err)
-        return ""
-    }
-    defer resp.Body.Close()
-    respBody := response{}
-    err = xml.NewDecoder(resp.Body).Decode(&respBody)
-    if err != nil {
-        log.Println("error decoding response")
-        log.Println(err)
-        return ""
-    }
-    if len(respBody.Posts) == 0 {
-        log.Println("error getting image")
-        return ""
-    }
-    return "https:" + respBody.Posts[0].File
+	// fetch the count if we don't have it
+	if count < 1 {
+		count = c.getCount(tags)
+		if count < 1 {
+			return ""
+		}
+	}
+	pid := rand.Intn(count)
+	requestUrl := baseUrl + tags + "&limit=1&pid=" + strconv.Itoa(pid)
+	log.Println("getting image from " + requestUrl)
+	resp, err := http.Get(requestUrl)
+	if err != nil {
+		log.Println("error fetching image")
+		log.Println(err)
+		return ""
+	}
+	defer resp.Body.Close()
+	respBody := response{}
+	err = xml.NewDecoder(resp.Body).Decode(&respBody)
+	if err != nil {
+		log.Println("error decoding response")
+		log.Println(err)
+		return ""
+	}
+	if len(respBody.Posts) == 0 {
+		log.Println("error getting image")
+		return ""
+	}
+	return "https:" + respBody.Posts[0].File
 }
 
 func (c CuteImage) getCount(tags string) int {
-    requestUrl := baseUrl + tags + "&limit=0"
-    log.Println("getting count from " + requestUrl)
-    resp, err := http.Get(requestUrl)
-    if err != nil {
-        log.Println("error fetching count")
-        log.Println(err)
-        return 0
-    }
-    defer resp.Body.Close()
-    respBody := response{}
-    err = xml.NewDecoder(resp.Body).Decode(&respBody)
-    if err != nil {
-        log.Println("error decoding response")
-        log.Println(err)
-        return 0
-    }
-    result, _ := strconv.Atoi(respBody.Count)
-    return result
+	requestUrl := baseUrl + tags + "&limit=0"
+	log.Println("getting count from " + requestUrl)
+	resp, err := http.Get(requestUrl)
+	if err != nil {
+		log.Println("error fetching count")
+		log.Println(err)
+		return 0
+	}
+	defer resp.Body.Close()
+	respBody := response{}
+	err = xml.NewDecoder(resp.Body).Decode(&respBody)
+	if err != nil {
+		log.Println("error decoding response")
+		log.Println(err)
+		return 0
+	}
+	result, _ := strconv.Atoi(respBody.Count)
+	return result
 }
--- meirl/meirl.go
+++ meirl/meirl.go
@@ -1,18 +1,18 @@
 package main
 
 import (
-    "log"
-    "io"
-    "io/ioutil"
-    "net"
-    "bufio"
-    "crypto/tls"
-    "regexp"
-    "time"
-    "os"
-    "strconv"
-    "ircbot/state"
-    "math/rand"
+	"bufio"
+	"crypto/tls"
+	"io"
+	"io/ioutil"
+	"ircbot/state"
+	"log"
+	"math/rand"
+	"net"
+	"os"
+	"regexp"
+	"strconv"
+	"time"
 )
 
 const server = "irc.rizon.net"
@@ -20,183 +20,184 @@ const port = "6697"
 const nickname = "meirlBot"
 const ident = "meirl"
 const realname = "a bot to post pic of yourself irl"
+
 var passwordBytes, _ = ioutil.ReadFile("password.txt")
 var password = string(passwordBytes)
 
-var pingRegex = regexp.MustCompile("^PING :(.*)$") // PING :message
-var motdEndRegex = regexp.MustCompile("^[^ ]* (?:376|422)") // :server 376 nick :End of /MOTD command.
-var vhostSetRegex = regexp.MustCompile("^[^ ]* 396 " + nickname) // :server 396 nick host :is now your visible host
-var privmsgRegex = regexp.MustCompile("^:([^!]*)![^ ]* PRIVMSG ([^ ]*) :(.*)$") // :nick!ident@host PRIVMSG #channel :message
-var inviteRegex = regexp.MustCompile("^([^ ]*) INVITE " + nickname + " :(.*)$") // :nick!ident@host INVITE nick :#channel
+var pingRegex = regexp.MustCompile("^PING :(.*)$")                                  // PING :message
+var motdEndRegex = regexp.MustCompile("^[^ ]* (?:376|422)")                         // :server 376 nick :End of /MOTD command.
+var vhostSetRegex = regexp.MustCompile("^[^ ]* 396 " + nickname)                    // :server 396 nick host :is now your visible host
+var privmsgRegex = regexp.MustCompile("^:([^!]*)![^ ]* PRIVMSG ([^ ]*) :(.*)$")     // :nick!ident@host PRIVMSG #channel :message
+var inviteRegex = regexp.MustCompile("^([^ ]*) INVITE " + nickname + " :(.*)$")     // :nick!ident@host INVITE nick :#channel
 var kickRegex = regexp.MustCompile("^([^ ]*) KICK ([^ ]*) " + nickname + " :(.*)$") // :nick!ident@host KICK #channel nick :message
-var banRegex = regexp.MustCompile("^[^ ]* 474 " + nickname + " ([^ ]*) :(.*)$") // :server 474 nick #channel :Cannot join channel (+b)
+var banRegex = regexp.MustCompile("^[^ ]* 474 " + nickname + " ([^ ]*) :(.*)$")     // :server 474 nick #channel :Cannot join channel (+b)
 
 type dbOp struct {
-    add bool
-    channel string
+	add     bool
+	channel string
 }
 
 func main() {
-    log.Println("starting up");
-    
-    t := time.Now()
-    ts := t.Format("Jan 2 2006 15-04-05 EDT")
-    
-    rand.Seed( time.Now().UTC().UnixNano())
-    
-    logFilename := "logs/" + ts + ".txt"
-    f, err := os.Create(logFilename)
-    if err != nil {
-        log.Fatalln(err);
-    }
-    defer f.Close()
-    // output to both stdout and the log file by default
-    logWriter := io.MultiWriter(os.Stdout, f)
-    log.SetOutput(logWriter)
-    fileOnlyLogger := log.New(f, "", log.Flags())
-    
-    socket, err := tls.Dial("tcp", server + ":" + port, &tls.Config{})
-    if err != nil {
-        log.Fatalln(err);
-    }
-    defer socket.Close()
-    log.Println("socket connected")
-    
-    errors := make(chan bool)
-    linesToSend := make(chan string, 5)
-    dbWrites := make(chan dbOp, 2)
-    go readLines(socket, errors, linesToSend, fileOnlyLogger, dbWrites)
-    go writeLines(socket, errors, linesToSend, fileOnlyLogger)
-    go manualInput(linesToSend)
-    go writeToDb(dbWrites)
-    linesToSend <- "NICK " + nickname
-    linesToSend <- "USER " + ident + " * 8 :" + realname
-    <- errors
+	log.Println("starting up")
+
+	t := time.Now()
+	ts := t.Format("Jan 2 2006 15-04-05 EDT")
+
+	rand.Seed(time.Now().UTC().UnixNano())
+
+	logFilename := "logs/" + ts + ".txt"
+	f, err := os.Create(logFilename)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer f.Close()
+	// output to both stdout and the log file by default
+	logWriter := io.MultiWriter(os.Stdout, f)
+	log.SetOutput(logWriter)
+	fileOnlyLogger := log.New(f, "", log.Flags())
+
+	socket, err := tls.Dial("tcp", server+":"+port, &tls.Config{})
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer socket.Close()
+	log.Println("socket connected")
+
+	errors := make(chan bool)
+	linesToSend := make(chan string, 5)
+	dbWrites := make(chan dbOp, 2)
+	go readLines(socket, errors, linesToSend, fileOnlyLogger, dbWrites)
+	go writeLines(socket, errors, linesToSend, fileOnlyLogger)
+	go manualInput(linesToSend)
+	go writeToDb(dbWrites)
+	linesToSend <- "NICK " + nickname
+	linesToSend <- "USER " + ident + " * 8 :" + realname
+	<-errors
 }
 
 // ensure db writes are done sequentially
 func writeToDb(dbWrites <-chan dbOp) {
-    for {
-        job := <-dbWrites
-        if job.add {
-            state.AddChannel(job.channel)
-        } else {
-            state.RemoveChannel(job.channel)
-        }
-    }
+	for {
+		job := <-dbWrites
+		if job.add {
+			state.AddChannel(job.channel)
+		} else {
+			state.RemoveChannel(job.channel)
+		}
+	}
 }
 
 // listen to stdin and send any lines over the socket
 func manualInput(linesToSend chan<- string) {
-    reader := bufio.NewReader(os.Stdin)
-    for {
-        text, err := reader.ReadString('\n')
-        if err != nil {
-            log.Fatalln(err)
-        }
-        linesToSend <- text
-    }
+	reader := bufio.NewReader(os.Stdin)
+	for {
+		text, err := reader.ReadString('\n')
+		if err != nil {
+			log.Fatalln(err)
+		}
+		linesToSend <- text
+	}
 }
 
 func readLines(socket net.Conn, errors chan<- bool, linesToSend chan<- string, logFile *log.Logger, dbWrites chan<- dbOp) {
-    reader := bufio.NewReader(socket)
-    for {
-        line, err := reader.ReadString('\n')
-        if err != nil {
-            log.Println(err)
-            errors <- true
-            continue
-        }
-        // remove the trailing \r\n
-        line = line[:len(line)-2]
-        logFile.Println(">>> " + line) // only print these to the log file, not to the default logger
-        processLine(line, linesToSend, dbWrites)
-    }
+	reader := bufio.NewReader(socket)
+	for {
+		line, err := reader.ReadString('\n')
+		if err != nil {
+			log.Println(err)
+			errors <- true
+			continue
+		}
+		// remove the trailing \r\n
+		line = line[:len(line)-2]
+		logFile.Println(">>> " + line) // only print these to the log file, not to the default logger
+		processLine(line, linesToSend, dbWrites)
+	}
 }
 
 func writeLines(socket net.Conn, errors chan<- bool, linesToSend <-chan string, logFile *log.Logger) {
-    writer := bufio.NewWriter(socket)
-    for {
-        line := <- linesToSend
-        logFile.Println("<<< " + line) // only print these to the log file, not to the default logger
-        _, err := writer.WriteString(line + "\n")
-        if err != nil {
-            log.Println(err)
-            errors <- true
-            continue
-        }
-        // make sure it actually gets sent
-        writer.Flush()
-    }
+	writer := bufio.NewWriter(socket)
+	for {
+		line := <-linesToSend
+		logFile.Println("<<< " + line) // only print these to the log file, not to the default logger
+		_, err := writer.WriteString(line + "\n")
+		if err != nil {
+			log.Println(err)
+			errors <- true
+			continue
+		}
+		// make sure it actually gets sent
+		writer.Flush()
+	}
 }
 
 func processLine(line string, linesToSend chan<- string, dbWrites chan<- dbOp) {
-    if pingRegex.MatchString(line) {
-        linesToSend <- "PONG :" + pingRegex.FindStringSubmatch(line)[1]
-    } else if motdEndRegex.MatchString(line) {
-        linesToSend <- "PRIVMSG nickserv :identify " + password
-    } else if vhostSetRegex.MatchString(line) {
-        log.Println("joining channels")
-        go joinChannels(linesToSend)
-    } else if privmsgRegex.MatchString(line) {
-        matches := privmsgRegex.FindStringSubmatch(line)
-        processPrivmsg(linesToSend, matches[1], matches[2], matches[3])
-    } else if inviteRegex.MatchString(line) {
-        matches := inviteRegex.FindStringSubmatch(line)
-        log.Println("joining " + matches[2] + ", invited by " + matches[1])
-        linesToSend <- "JOIN " + matches[2]
-        dbWrites <- dbOp{true, matches[2]}
-    } else if kickRegex.MatchString(line) {
-        matches := kickRegex.FindStringSubmatch(line)
-        log.Println("kicked from " + matches[2] + " by " + matches[1] + " because " + matches[3])
-        dbWrites <- dbOp{false, matches[2]}
-    } else if banRegex.MatchString(line) {
-        matches := banRegex.FindStringSubmatch(line)
-        log.Println("can't join " + matches[1] + " because " + matches[2])
-        dbWrites <- dbOp{false, matches[2]}
-    }
+	if pingRegex.MatchString(line) {
+		linesToSend <- "PONG :" + pingRegex.FindStringSubmatch(line)[1]
+	} else if motdEndRegex.MatchString(line) {
+		linesToSend <- "PRIVMSG nickserv :identify " + password
+	} else if vhostSetRegex.MatchString(line) {
+		log.Println("joining channels")
+		go joinChannels(linesToSend)
+	} else if privmsgRegex.MatchString(line) {
+		matches := privmsgRegex.FindStringSubmatch(line)
+		processPrivmsg(linesToSend, matches[1], matches[2], matches[3])
+	} else if inviteRegex.MatchString(line) {
+		matches := inviteRegex.FindStringSubmatch(line)
+		log.Println("joining " + matches[2] + ", invited by " + matches[1])
+		linesToSend <- "JOIN " + matches[2]
+		dbWrites <- dbOp{true, matches[2]}
+	} else if kickRegex.MatchString(line) {
+		matches := kickRegex.FindStringSubmatch(line)
+		log.Println("kicked from " + matches[2] + " by " + matches[1] + " because " + matches[3])
+		dbWrites <- dbOp{false, matches[2]}
+	} else if banRegex.MatchString(line) {
+		matches := banRegex.FindStringSubmatch(line)
+		log.Println("can't join " + matches[1] + " because " + matches[2])
+		dbWrites <- dbOp{false, matches[2]}
+	}
 }
 
 func joinChannels(linesToSend chan<- string) {
-    for _, ch := range state.GetChannels() {
-        linesToSend <- "JOIN " + ch
-    }
+	for _, ch := range state.GetChannels() {
+		linesToSend <- "JOIN " + ch
+	}
 }
 
-var cooldown = make(map[string]int); // map of "#channel nick" to cooldown
+var cooldown = make(map[string]int) // map of "#channel nick" to cooldown
 var cuteImage = CuteImage{}
 
 func processPrivmsg(linesToSend chan<- string, nick string, channel string, msg string) {
-    chanNick := channel + " " + nick
-    if cuteImage.checkForMatch(msg) {
-        // reply to the channel or to a pm
-        sendTo := ""
-        if channel[:1] == "#" && cooldown[chanNick] == 0 {
-            sendTo = channel
-            cooldown[chanNick] = 5
-            log.Println("cd for " + chanNick + " is " + strconv.Itoa(cooldown[chanNick]))
-        } else if (channel == nickname) {
-            // pm, reply to the nick
-            sendTo = nick
-        }
-        
-        if (sendTo != "") {
-            go sendImage(linesToSend, sendTo, msg, cuteImage)
-        }
-    } else if (cooldown[chanNick] != 0) {
-        cooldown[chanNick] -= 1
-        log.Println("cd for " + chanNick + " is " + strconv.Itoa(cooldown[chanNick]))
-        if cooldown[chanNick] == 0 {
-            delete(cooldown, chanNick)
-        }
-    }
+	chanNick := channel + " " + nick
+	if cuteImage.checkForMatch(msg) {
+		// reply to the channel or to a pm
+		sendTo := ""
+		if channel[:1] == "#" && cooldown[chanNick] == 0 {
+			sendTo = channel
+			cooldown[chanNick] = 5
+			log.Println("cd for " + chanNick + " is " + strconv.Itoa(cooldown[chanNick]))
+		} else if channel == nickname {
+			// pm, reply to the nick
+			sendTo = nick
+		}
+
+		if sendTo != "" {
+			go sendImage(linesToSend, sendTo, msg, cuteImage)
+		}
+	} else if cooldown[chanNick] != 0 {
+		cooldown[chanNick] -= 1
+		log.Println("cd for " + chanNick + " is " + strconv.Itoa(cooldown[chanNick]))
+		if cooldown[chanNick] == 0 {
+			delete(cooldown, chanNick)
+		}
+	}
 }
 
 func sendImage(linesToSend chan<- string, sendTo string, msg string, img CuteImage) {
-    str, url := img.getImageForMessage(msg);
-    if url != "" {
-        newMsg := "PRIVMSG " + sendTo + " " + ":" + str + " " + url
-        log.Println("sending image: " + newMsg);
-        linesToSend <- newMsg
-    }
+	str, url := img.getImageForMessage(msg)
+	if url != "" {
+		newMsg := "PRIVMSG " + sendTo + " " + ":" + str + " " + url
+		log.Println("sending image: " + newMsg)
+		linesToSend <- newMsg
+	}
 }
